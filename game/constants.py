8a1e84b8883b8c6867663f65bc3d8c460580dec3
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2023-01-13 19:32:47 -0330

    changed movement system

---
+++ game/constants.py
@@ -0,0 +1,17 @@
+GameMasterSettings = {
+    "GRAVITY": (0, 0),
+    "DEFAULT_DAMPING": 0.2,
+    
+    "PLAYER_FRICTION": 1.0,
+    "PLAYER_MASS": 20.0,
+    "PLAYER_MAX_SPEED": 1400,
+    "PLAYER_MOVE_FORCE": 1000,
+    
+    "BULLET_FORCE": 4500,
+    "BULLET_MASS": 0.1,
+    "BULLET_STOP_SPEED": 150,
+    "FIRE_COOLDOWN": 8,
+    "CARGO_MAX_SPEED":800,
+    "CARGO_MOVE_FORCE": 6000,
+    "CARGO_MASS": 600
+}
